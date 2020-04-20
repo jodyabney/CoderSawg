76894038ab3b14cd20553fb7ae2b028bf4e22020
Jody Abney <jodyabney@gmail.com>
2020-04-20 19:01:28 -0400

    Complete Week 6 Lesson App: CoderSwag - Collection View Cell

---
+++ CoderSwag/View/ProductViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductViewCell.swift
+//  CoderSwag
+//
+//  Created by Jody Abney on 4/20/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
