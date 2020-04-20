40ee7a765161b00091c479e6a9fe74f52ebad08c
Jody Abney <jodyabney@gmail.com>
2020-04-20 19:30:57 -0400

    Complete Week 6 Lesson App: CoderSwag - Creating Product Model

---
+++ CoderSwag/Model/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  CoderSwag
+//
+//  Created by Jody Abney on 4/20/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+}
