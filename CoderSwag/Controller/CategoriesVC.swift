ee6db1817dc051bc84aa2e0e19eb43b8d85993c4
Jody Abney <jodyabney@gmail.com>
2020-04-20 16:50:02 -0400

    Complete Week 6 Lesson App: CoderSwag - Table Cell

--- CoderSwag/Controller/CategoriesVC.swift
+++ CoderSwag/Controller/CategoriesVC.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class CategoriesVC: UIViewController {
-    
+class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
     @IBOutlet weak var categoryTableView: UITableView!
 
     override func viewDidLoad() {
@@ -18,5 +18,22 @@ class CategoriesVC: UIViewController {
     }
 
 
+    
+    //MARK: - TableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryViewCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryViewCell()
+        }
+    }
+    
 }
 
